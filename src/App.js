13c71b0139602e43aaf4800c261404a3958bbdb2
Rodrigo Sauco <rodrigo.sauco@globallogic.com>
2021-04-28 22:52:26 -0300

    Se agregaron componentes de muestreo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route , Switch} from 'react-router';
 import './App.scss';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
