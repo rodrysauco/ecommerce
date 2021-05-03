ffbd60d56c16a48da73b34b4740a04d5db3c7972
Rodrigo Sauco <rodrigo.sauco@globallogic.com>
2021-05-03 17:26:49 -0300

    Redirect a home y se agrega tipo de accion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -44,15 +44,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/login' component={LoginPage} />
+          <Route
+            exact
+            path='/login'
+            render={
+              () => this.props.currentUser ?
+                (<Redirect to='/' />) :
+                (<LoginPage />)}
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
