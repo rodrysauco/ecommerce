e09d135775589f8ee858cd221da0556eb17b650c
Rodrigo Sauco <rodrigo.sauco@globallogic.com>
2021-05-03 22:39:23 -0300

    Se agrega item del dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
 
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
   <div className="cart-dropdown">
-    <div className="cart-dropdown__items"></div>
+    <div className="cart-dropdown__items">
+    {cartItems.map(cartItem =>
+      <CartItem key={cartItem.id} item={cartItem}/>)}
+    </div>
     <Button> Ir al Carrito</Button>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } })=>({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
